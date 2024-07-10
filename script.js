b37cb23f1b0a15d3e0a0e62c2faaa765e776efa2
Telenchak Oleksiy <letel315@gmail.com>
2024-07-11 00:17:10 +0300

    rewrite faq section

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-const emergencyStepsItems = document.querySelectorAll('.emergency-steps__item')
-const emergencyStepsHoverContent = document.querySelector('.emergency-steps__content')
+const emergencyStepsItems = document.querySelectorAll(".emergency-steps__item");
+const emergencyStepsHoverContent = document.querySelector(
+  ".emergency-steps__content"
+);
 
-emergencyStepsItems.forEach(item => {
-        item.addEventListener('mouseover', () => {
-            const rect = item.getBoundingClientRect();
-        });
-        item.removeEventListener('mouseout', () => {
-        });
-    }
-)
-
-document.addEventListener("DOMContentLoaded", () => {
-    const questions = document.querySelectorAll('.faq__question');
-    questions.forEach(question => {
-        question.addEventListener('click', () => {
-            const answer = question.nextElementSibling;
-            if (answer.style.display === "block") {
-                answer.style.display = "none";
-            } else {
-                answer.style.display = "block";
-            }
-        });
-    });
+emergencyStepsItems.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    const rect = item.getBoundingClientRect();
+  });
+  item.removeEventListener("mouseout", () => {});
 });
-
